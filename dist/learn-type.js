0f5472dddcb15122472b4b4144cbe7f2abf6a4ff
Abu Raihan <aburaihanrahmani@gmail.com>
2025-03-24 19:48:55 +0600

    learn-type file added

---
+++ dist/learn-type.js
@@ -0,0 +1,5 @@
+"use strict";
+let number = 234;
+let nam = "allah";
+let address = `${nam} abu raihan`;
+let bul = false;
---
+++ src/learn-type.ts
@@ -0,0 +1,4 @@
+let number: number = 234;
+let nam: string = "allah";
+let address: string = `${nam} abu raihan`;
+let bul: boolean = false;
